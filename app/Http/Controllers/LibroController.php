ab8f1fb06989865559c89a40fea36ddbb7870d7b
elian gonzalez <elianngonzalez7@gmail.com>
2022-03-18 23:30:46 -0300

    only one

--- app/Http/Controllers/LibroController.php
+++ app/Http/Controllers/LibroController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Libro;
 
+use Carbon\Carbon;
+
 class LibroController extends Controller{
 
     public function index(){
@@ -11,11 +13,25 @@ class LibroController extends Controller{
         return response()->json($librosDatos);
     }
 
+    public function onliOne($id){
+        $libro = Libro::find($id);
+        return response()->json($libro);
+    }
+
     public function save(Request $request){
         $libro = new Libro();
+        
+        if($request->hasFile("imagen")){
+            $nombreArchivo = $request->file("imagen")->getClientOriginalName();
+
+            $nuevoNombreArchivo = Carbon::now()->timestamp."_".$nombreArchivo;
+        }
+
         $libro->titulo = $request->input('titulo');
         $libro->imagen = $request->input('imagen');
         // $libro->save();
-        return response()->json($libro);
+        return response()->json($nuevoNombreArchivo);
     }
+
+    //public function delete (Request $request){}
 }
